e28c1902dd814f138c490bd6185d0aa97e247d8a
William <William@163.com>
2017-12-19 11:40:07 +0800

    更新了代码

--- Calculator/src/CalculatorFrame.java
+++ Calculator/src/CalculatorFrame.java
@@ -4,15 +4,10 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Stack;
 
 public class CalculatorFrame extends JFrame {
     private JTextField text = null;
     private JButton ACbutton = null;
-    private Double resultValue = 0.0;
-    private String nowOpr = null;
-    private int countInput = 0;
-    private int isCleanFlag = 0;
     private static final int PRE_WIDTH = 200;
     private static final int PRE_HEIGHT = 300;
     private String[] nOp = {"AC","←","%","÷","7","8","9","×","4","5","6","-","1","2","3","+","0","00",".","="};
@@ -28,12 +23,10 @@ public class CalculatorFrame extends JFrame {
         //设置布局
         this.setLayout(new BorderLayout());
         //设置内部组件
-
         //1.设置北部面板
         JPanel nPanel = new JPanel();
         nPanel.setLayout(new FlowLayout());
         nPanel.add(getTextField());
-
         //2.设置南部面板
         JPanel sPanel = new JPanel();
         sPanel.setSize(PRE_WIDTH,PRE_HEIGHT/3);
@@ -50,134 +43,24 @@ public class CalculatorFrame extends JFrame {
                             if(!(text.getText().equals("0"))){
                                 text.setText("0");
                             }
-                            resultValue = 0.0;
-//                            countInput = 0;
                         }else if(NumBtn.getText().equals("=")) {
                             Double val;
-                            try {
-                                val= Double.valueOf(text.getText());
-                            }catch(NumberFormatException ex){
-                                ex.printStackTrace();
-                                val = 0.0;
-                            }
-//                            switch(nowOpr){
-//                                case "+":
-//                                    resultValue = NumberOpration.plus(resultValue,val);
-//                                    break;
-//                                case "-":
-//                                    resultValue = NumberOpration.substract(resultValue,val);
-//                                    break;
-//                                case "×":
-//                                    resultValue = NumberOpration.multiply(resultValue,val);
-//                                    break;
-//                                case "÷":
-//                                    resultValue = NumberOpration.divide(resultValue,val);
-//                                    break;
-//                                case "%":
-//                                    if(resultValue.intValue() == resultValue && val.intValue() == val){
-//                                        Integer value;
-//                                        value = NumberOpration.remainder(resultValue.intValue(),val.intValue());
-//                                        resultValue = value.doubleValue();
-//                                    }
-//                                    break;
-//                            }
-                            takeCalculate(nowOpr,val);
-                            text.setText(resultValue.toString());
+                            String exp = text.getText();
+                            //通过表达式运算结果
+                            val = NumberOpration.calculateExpression(exp);
+                            text.setText(val.toString());
                         }else if(NumBtn.getText().equals("←")){
                             //点按退格按键退格
                             String str = text.getText();
                             str = str.substring(0,str.length()-1);
                             text.setText(str);
-                        }else if(NumBtn.getText().equals("+")||
-                                 NumBtn.getText().equals("-")||
-                                 NumBtn.getText().equals("×")||
-                                 NumBtn.getText().equals("÷")||
-                                 NumBtn.getText().equals("%")){
-                            nowOpr = NumBtn.getText();
-                            Double val;
-
-                                try {
-                                    val= Double.valueOf(text.getText());
-                                }catch(NumberFormatException ex){
-                                    ex.printStackTrace();
-                                    val = 0.0;
-                                }
-                                takeCalculate(nowOpr,val);
-//                                text.setText(resultValue.toString());
-
-
-                            //执行"等于"的函数
-
-//                            try {
-//                                val= Double.valueOf(text.getText());
-//                            }catch(NumberFormatException ex){
-//                                ex.printStackTrace();
-//                                val = 0.0;
-//                            }
-//                            takeCalculate(nowOpr,val);
-//                            text.setText(resultValue.toString());
-
-
-
-                            //当前运算符
-
-                            String opr = NumBtn.getText();
-                            switch(opr){
-                                case "+":
-                                    resultValue = NumberOpration.plus(resultValue,val);
-//                                    countInput++;
-                                    break;
-                                case "-":
-                                    resultValue = NumberOpration.substract(resultValue,val);
-//                                    countInput++;
-                                    break;
-                                case "×":
-                                    if(countInput == 0){
-                                        resultValue = val;
-                                    }else{
-                                        resultValue = NumberOpration.multiply(resultValue,val);
-                                    }
-//                                    countInput++;
-                                    break;
-                                case "÷":
-                                    if(countInput == 0){
-                                        resultValue = val;
-                                    }else{
-                                        resultValue = NumberOpration.divide(resultValue,val);
-                                    }
-
-//                                    countInput++;
-                                    break;
-                                case "%":
-                                    if(countInput == 0){
-                                        resultValue = val;
-                                    }else{
-                                        if(resultValue.intValue() == resultValue && val.intValue() == val){
-                                            Integer value;
-                                            value = NumberOpration.remainder(resultValue.intValue(),val.intValue());
-                                            System.out.print(value);
-                                            resultValue = value.doubleValue();
-                                        }
-                                    }
-//                                    countInput++;
-
-
-
-                                    break;
-                            }
-                            countInput++;
-                            isCleanFlag = 1;
                         }else{
-                            if(isCleanFlag == 1){
-                                text.setText("");
-                            }
                             String str = text.getText();
                             if(text.getText().equals("0")){
                                 str = "";
                             }
                             str = str + NumBtn.getText();
                             text.setText(str);
-                            isCleanFlag = 0;
                         }
                 }
             });
@@ -213,40 +96,7 @@ public class CalculatorFrame extends JFrame {
         });
         return text;
     }
-
-private void takeCalculate(String opr,Double value){
-    switch(opr){
-        case "+":
-            resultValue = NumberOpration.plus(resultValue,value);
-            break;
-        case "-":
-            resultValue = NumberOpration.substract(resultValue,value);
-            break;
-        case "×":
-            resultValue = NumberOpration.multiply(resultValue,value);
-            break;
-        case "÷":
-            resultValue = NumberOpration.divide(resultValue,value);
-            break;
-        case "%":
-            if(resultValue.intValue() == resultValue && value.intValue() == value){
-                Integer val;
-                val = NumberOpration.remainder(resultValue.intValue(),value.intValue());
-                resultValue = val.doubleValue();
-            }
-            break;
-    }
-
-
-}
-
-
-
-
-
-
     public static void main(String args[]){
         new CalculatorFrame().setVisible(true);
-
     }
 }
--- Calculator/src/NumberOpration.java
+++ Calculator/src/NumberOpration.java
@@ -4,16 +4,16 @@ import java.util.Stack;
  * Created by william on 2017/11/11.
  */
 public class NumberOpration {
-    public static Double plus(double a,double b){
+    public static Double plus(Double a,Double b){
         return a + b;
     }
-    public static Double substract(double a,double b){
+    public static Double substract(Double a,Double b){
         return a - b;
     }
-    public static Double multiply(double a,double b){
+    public static Double multiply(Double a,Double b){
         return a * b;
     }
-    public static Double divide(double a,double b){
+    public static Double divide(Double a,Double b){
         double c;
         try {
             c = a / b;
@@ -23,43 +23,61 @@ public class NumberOpration {
         }
         return c;
     }
-    public static int remainder(int a,int b){
-        int c;
+    public static Double remainder(Double a,Double b){
+        if(a.intValue() != a && b.intValue() != b){
+            return 0.0;
+        }
+        Integer c;
         try {
-            c = a % b;
+            c = a.intValue() % b.intValue();
         }catch(ArithmeticException e){
             e.printStackTrace();
             c = 0;
         }
-        return c;
+        return c.doubleValue();
     }
 
-    public boolean comparePro(String inputOper, String sTopOperator) {
-        //如果栈顶运算符为最括号，直接将输入运算符压栈
-        if ("(".equals(sTopOperator)) {
-            return true;
-        }
+    private static boolean comparePro(String inputOper, String sTopOperator) {
         // 如果当前栈顶的操作运算符为“+”或“-”，并且输入运算符为“*”或“/”时，输入的运算符优先级大
-        if (("*".equals(inputOper) || "/".equals(inputOper))
-                && ("+".equals(sTopOperator) || "-".equals(sTopOperator))) {
+        if ((inputOper.equals("×") || inputOper.equals("÷")||inputOper.equals("%"))
+                && (sTopOperator.equals("+") || sTopOperator.equals("-"))) {
             return true;
         }
         return false;
     }
 
+    private static Double calculate(Double val1,Double val2,String opr){
+        switch (opr){
+            case "+":
+                return plus(val1,val2);
+            case "-":
+                return substract(val1,val2);
+            case "×":
+                return multiply(val1,val2);
+            case "÷":
+                return divide(val1,val2);
+            case "%":
+                return remainder(val1,val2);
+            default:
+                return 0.0;
+        }
+    }
 
-    private Double calculateExpression(String exp){
+
+
+    public static Double calculateExpression(String exp){
         Stack<String> oprStack = new Stack<>();
         Stack<String>numStack = new Stack<>();
         Double result;
         for(int i = 0;i<exp.length();i++){
             Character c1 = exp.charAt(i);
+
             if(c1>='0'&&c1<='9'){
                 String s = "";
                 s = s + c1.toString();
-                for(int j = i+1;;j++){
+                for(int j = i+1;j<exp.length();j++){
                     Character c2 = exp.charAt(j);
-                    if(c1>='0'&&c1<='9'){
+                    if(c2>='0'&&c2<='9'){
                         s = s + c2.toString();
                         i = j;
                     }else{
@@ -68,12 +86,34 @@ public class NumberOpration {
                 }
                 numStack.push(s);
             }else{
-                if(comparePro(c1.toString(),oprStack.peek())){
+                if(oprStack.isEmpty() == true){
+                    oprStack.push(c1.toString());
+                }
+                //输入运算符优先级高于栈顶运算符
+                else if(comparePro(c1.toString(),oprStack.peek())){
                     //入栈
                     oprStack.push(c1.toString());
+                }else if(!comparePro(c1.toString(),oprStack.peek())){
+                    //取数据栈栈顶两个元素进行运算
+                    Double val1 = Double.valueOf(numStack.pop());
+                    Double val2 = Double.valueOf(numStack.pop());
+                    Double res = calculate(val2,val1,c1.toString());
+                    numStack.push(res.toString());
+
+                }else{
+                    break;
                 }
             }
         }
+        while(oprStack.isEmpty() != true){
+            Double val1 = Double.valueOf(numStack.pop());
+            Double val2 = Double.valueOf(numStack.pop());
+            String opr = oprStack.pop();
+            numStack.push(calculate(val2,val1,opr).toString());
+        }
+        //oprStack栈为空则最后结果就是numStack的栈顶元素
+        result = Double.valueOf(numStack.pop());
+        return result;
     }
 
 }
